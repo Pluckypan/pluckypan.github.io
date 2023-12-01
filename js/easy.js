39a32f699321cfa361bd110090a7300ba15f26de
plucky <plucky@echo.engineer>
2023-12-01 09:46:47 +0800

    Auto generatted by eedoc at 1701395201150

---
+++ js/easy.js
@@ -0,0 +1,38 @@
+function toTimestamp(t) {
+    return new Date(t).getTime();
+}
+
+Array.prototype.shuffle = function () {
+    let array = JSON.parse(JSON.stringify(this));
+    var m = array.length,
+        t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+    return array;
+};
+
+Array.prototype.postSortBy = function (hot) {
+    let array = [];
+    if (hot) {
+        array = this.sort((a, b) => {
+            let bb = b.banner ? 1582201220000 : 0;
+            let ab = a.banner ? 1582201220000 : 0;
+            let t = (toTimestamp(b.update_time) + bb) - (toTimestamp(a.update_time) + ab);
+            return t;
+        });
+    } else {
+        array = this.sort((a, b) => toTimestamp(b.update_time) - toTimestamp(a.update_time));
+    }
+    return array;
+};
+
+Array.prototype.checkSubset = function (subArray) {
+    let parentArray = this;
+    return subArray.every((el) => {
+        return parentArray.includes(el)
+    })
+}
